ff539c86f216859c605157cf0cb3ce9533d15e94
Cynthia6 <geri_tod@abv.bg>
2017-04-07 22:45:55 +0300

    Adding the last problems of arrays exercises

---
+++ TECHNOLOGY FUNDAMENTALS-Ext/Arrays-Labs-Exercises/10.Balistics Training/Ballistics.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Balistics_Training
+{
+    class Ballistics
+    {
+        static void Main()
+        {
+            var cordinates = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var sequence = Console.ReadLine().Split();
+            var result = new int[2];
+
+            for (int i = 0, j = 1; i < sequence.Length; i++, j++)
+            {
+                if (sequence[i] == "up")
+                {
+                    result[1] += int.Parse(sequence[j]);
+                }
+                else if (sequence[i] == "down")
+                {
+                    result[1] -= int.Parse(sequence[j]);
+                }
+                else if (sequence[i] == "left")
+                {
+                    result[0] -= int.Parse(sequence[j]);
+                }
+                else if (sequence[i] == "right")
+                {
+                    result[0] += int.Parse(sequence[j]);
+                }
+            }
+
+            if (cordinates[0] == result[0] && cordinates[1] == result[1])
+            {
+                Console.WriteLine("firing at [{0}, {1}]", result[0], result[1]);
+                Console.WriteLine("got 'em!");
+            }
+            else
+            {
+                Console.WriteLine("firing at [{0}, {1}]", result[0], result[1]);
+                Console.WriteLine("better luck next time...");
+            }
+
+        }
+    }
+}
--- TECHNOLOGY FUNDAMENTALS-Ext/Arrays-Labs-Exercises/8.Array Symmetry/Symmetry.cs
+++ TECHNOLOGY FUNDAMENTALS-Ext/Arrays-Labs-Exercises/8.Array Symmetry/Symmetry.cs
@@ -10,8 +10,24 @@ namespace _8.Array_Symmetry
     {
         public static void Main()
         {
-
-
+            string[] words = Console.ReadLine().Split(' ').ToArray();
+            bool isSymmetric = true;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] != words[words.Length-i-1])
+                {
+                    isSymmetric = false;
+                    break;
+                }
+            }
+            if (isSymmetric)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
