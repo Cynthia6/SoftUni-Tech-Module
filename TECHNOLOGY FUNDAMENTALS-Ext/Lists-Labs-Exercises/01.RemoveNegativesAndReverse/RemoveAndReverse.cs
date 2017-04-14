243b6693f99547effff761e6c343d5cb9d23f1d2
Cynthia6 <geri_tod@abv.bg>
2017-04-14 19:49:41 +0300

    uploading lists labs

---
+++ TECHNOLOGY FUNDAMENTALS-Ext/Lists-Labs-Exercises/01.RemoveNegativesAndReverse/RemoveAndReverse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Read a list of integers, remove all negative numbers from it and print the remaining elements in reversed order. 
+//In case of no elements left in the list, print “empty”.
+namespace _01.RemoveNegativesAndReverse
+{
+    public class RemoveAndReverse
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            numbers.Reverse();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+            }
+            if (numbers.Count != 0) Console.WriteLine(string.Join(" ", numbers));
+            else Console.WriteLine("empty");
+        }
+    }
+}
---
+++ TECHNOLOGY FUNDAMENTALS-Ext/Lists-Labs-Exercises/03.Sum Adjacent Equal Numbers/Sum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Write a program to sum all adjacent equal numbers in a list of decimal numbers, starting from left to right.
+//	After two numbers are summed, the obtained result could be equal to some of its neighbors and should be summed as well(see the examples below).
+//	Always sum the leftmost two equal neighbors(if several couples of equal neighbors are available).
+
+namespace _03.Sum_Adjacent_Equal_Numbers
+{
+    public class Sum
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i] = numbers[i] + numbers[i + 1];
+                    numbers.RemoveAt(i + 1);
+                    i -= 2;
+                }
+                if (i < -1)
+                {
+                    i = -1;
+                }
+                
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+    }
+}
