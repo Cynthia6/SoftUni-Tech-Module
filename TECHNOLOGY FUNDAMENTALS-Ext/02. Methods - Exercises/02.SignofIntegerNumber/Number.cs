65fce536c9349ef7b0c11ff51ad319c793fd20a0
Cynthia6 <geri_tod@abv.bg>
2017-05-05 03:07:54 +0300

    adding all Methods-exercises and labs

---
+++ TECHNOLOGY FUNDAMENTALS-Ext/02. Methods - Exercises/02.SignofIntegerNumber/Number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//Create a method that prints the sign of an integer number n.
+namespace _02.SignofIntegerNumber
+{
+    public class Number
+    {
+        public static void Main()
+        {
+            int n =int.Parse(Console.ReadLine());
+
+            PrintSign(n);
+
+        }
+        public static void PrintSign(int n)
+        {
+            if (n < 0) Console.WriteLine($"The number {n} is negative.");
+            else if(n == 0) Console.WriteLine($"The number {n} is zero.");
+            else if (n > 0) Console.WriteLine($"The number {n} is positive.");
+        }
+    }
+}
---
+++ TECHNOLOGY FUNDAMENTALS-Ext/02. Methods - Exercises/1.Hello, Name!/Name.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//Write a method that receives a name as parameter and prints on the console. “Hello, <name>!”
+namespace _1.Hello__Name_
+{
+    public class Name
+    {
+        public static void Main()
+        {
+
+            string name = Console.ReadLine();
+            Console.WriteLine(HelloName(name));
+        }
+        public static string HelloName(string a)
+        {
+            string result = $"Hello, {a}!";
+            return result;
+        }
+    }
+}
